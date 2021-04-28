22634494c4c209009e25e04aa4235d72fa62ef23
KevinF <kevinoflannagain@gmail.com>
2021-04-28 14:32:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,16 +35,46 @@ let johnWeight = 92;
 let johnHeight = 1.95;
 */
 
-let johnWeight = 92;
-let johnHeight = 1.95;
-let johnBMI = johnWeight / johnHeight ** 2;
-console.log(johnBMI);
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+// let johnBMI = johnWeight / johnHeight ** 2;
+// console.log(johnBMI);
 
 
-let markWeight = 78;
-let markHeight = 1.69;
-let markBMI = markWeight / markHeight ** 2;
-let markHigherBMI = markBMI > johnBMI;
-console.log(markBMI);
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let markBMI = markWeight / markHeight ** 2;
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI);
 
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
+
+//---------------------------------STRINGs and template literals----------------------------------
+/*
+const firstname = 'Kev';
+const job = 'engineer';
+const byear = 1995;
+const year = 2021;
+
+const kev = "I'm " + firstname + ', a ' + (year - byear) + ' year old ' + job + '!';
+console.log(kev)
+
+//template literal, template string
+const kevNew = `I'm ${firstname}, a ${year - byear} year old ${job}!`
+console.log(kevNew)
+
+console.log(`normal string`)
+console.log('string with \n\
+multiple \n\lines')
+
+console.log(`string
+multiple
+lines`)
+*/
+
+const age = 19;
+const isOldEnough = age >= 18;
+
+if () {
+
+}
