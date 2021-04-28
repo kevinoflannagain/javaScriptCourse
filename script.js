c1f840c64ae8deee135231b4e0c8db3819671ea1
KevinF <kevinoflannagain@gmail.com>
2021-04-28 14:21:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-let js = 'amazing';
-if (js === 'amazing') alert('js is fun');
 
-40 + 8 + 23 - 10;
+/*
+let js = 'amazing';
+
 console.log(40 + 8 + 23 - 10);
+console.log('jonas')
+
+let firstName = 'Jonas';
+console.log('jonas')
+
+
+let country = 'Ireland'
+console.log(country)
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+*/
+/*
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'oflannagain';
+
+const now = 2021;
+const ageKev = now - 1995;
+console.log(ageKev * 2);
+console.log(ageKev + ' ' + job);
+
+const markWeight = 78;
+const markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+*/
+
+let johnWeight = 92;
+let johnHeight = 1.95;
+let johnBMI = johnWeight / johnHeight ** 2;
+console.log(johnBMI);
+
+
+let markWeight = 78;
+let markHeight = 1.69;
+let markBMI = markWeight / markHeight ** 2;
+let markHigherBMI = markBMI > johnBMI;
+console.log(markBMI);
+
+console.log(markHigherBMI);
