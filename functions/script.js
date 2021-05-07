37f6091173713725421a5d97bea7b84e7750b8da
KevinF <kevinoflannagain@gmail.com>
2021-05-07 14:12:42 +0100

    map

--- functions/script.js
+++ functions/script.js
@@ -106,7 +106,6 @@ function containsDuplicates(a) {
         // add entry in object with the element as key
         map[a[i]] = true;
     }
-
     return duplicates
 }
 
@@ -167,4 +166,52 @@ function makeArrayConsecutive2(statues) {
     console.log(Math.min(...statues))
 
     return arr.length - statues.length
+}
+
+// ---------------------binary search iterative------------------------
+// Iterative function to implement Binary Search
+let iterativeFunction = function (arr, x) {
+
+    let start = 0, end = arr.length - 1;
+
+    // Iterate while start not meets end
+    while (start <= end) {
+
+        // Find the mid index
+        let mid = Math.floor((start + end) / 2);
+
+        // If element is present at mid, return True
+        if (arr[mid] === x) return true;
+
+        // Else look in left or right half accordingly
+        else if (arr[mid] < x)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    return false;
+}
+
+//----------------------binary search recursive----------------------
+let recursiveFunction = function (arr, x, start, end) {
+
+    // Base Condition
+    if (start > end) return false;
+
+    // Find the middle index
+    let mid = Math.floor((start + end) / 2);
+
+    // Compare mid with given key x
+    if (arr[mid] === x) return true;
+
+    // If element at mid is greater than x,
+    // search in the left half of mid
+    if (arr[mid] > x)
+        return recursiveFunction(arr, x, start, mid - 1);
+    else
+
+        // If element at mid is smaller than x,
+        // search in the right half of mid
+        return recursiveFunction(arr, x, mid + 1, end);
 }
